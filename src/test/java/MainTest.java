cf82e9d474cc818cdcf810dcdae374ef84f1266f
vitaliy kotov <v.kotov@corp.badoo.com>
2019-11-28 13:09:09 +0300

    remove Singleton

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,6 +1,3 @@
-import core.Conf;
-import core.Config;
-import core.Mode;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -13,36 +10,22 @@ public class MainTest {
 
     @BeforeTest
     public void start() {
-        this.driver.get(
-                Conf.getInstance(Mode.DEVEL).getUrl()
-        );
-
         this.driver = new ChromeDriver();
     }
 
     @Test
     public void testA() {
-        this.driver.get(
-                Config.getInstance().getUrl()
-        );
+        this.driver.get("https://ya.ru");
     }
 
     @Test
     public void testB() {
-        this.driver.get(
-                Conf.getInstance().getUrl()
-        );
+        this.driver.get("https://yandex.ru");
     }
 
     @AfterClass
     public void finish() {
         this.driver.close();
         this.driver.quit();
-    }
-
-    protected void openMainUrl() {
-        this.driver.get(
-                Config.getInstance().getUrl()
-        );
     }
 }
