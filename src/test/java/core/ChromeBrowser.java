3715508ca942ef18f57e518fcdb720a562607f0b
vitaliy kotov <v.kotov@corp.badoo.com>
2019-11-28 20:45:54 +0300

    add browser options

--- src/test/java/core/ChromeBrowser.java
+++ src/test/java/core/ChromeBrowser.java
@@ -1,10 +1,22 @@
 package core;
 
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class ChromeBrowser extends Browser {
     public RemoteWebDriver launchBrowser() {
-        return new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+        // Add the WebDriver proxy capability.
+        Proxy proxy = new Proxy();
+        proxy.setHttpProxy("myhttpproxy:3337");
+        options.setCapability("proxy", "");
+
+        // Add a ChromeDriver-specific capability.
+        ChromeDriver driver = new ChromeDriver(options);
+
+        return new ChromeDriver(options);
     }
 }
--- src/test/java/core/FirefoxBrowser.java
+++ src/test/java/core/FirefoxBrowser.java
@@ -1,10 +1,15 @@
 package core;
 
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class FirefoxBrowser extends Browser {
     public RemoteWebDriver launchBrowser() {
-        return new FirefoxDriver();
+        FirefoxOptions options = new FirefoxOptions()
+                .addPreference("browser.startup.page", 1)
+                .addPreference("browser.startup.homepage", "https://www.google.co.uk");
+
+        return new FirefoxDriver(options);
     }
 }
